c0f4c68f57eb2a6da35b171eab2c58841e5326f2
MatheusEduardoPy <matheuslopez.net@gmail.com>
2024-04-22 20:38:57 -0300

    Usei getenv, programei static, media e setei infos

--- djangoapp/project/settings.py
+++ djangoapp/project/settings.py
@@ -15,18 +15,25 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+# /data/web/static
+# /data/web/media
+DATA_DIR = BASE_DIR.parent / 'data' / 'web'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-l2&x48a@w@!*1n6kz(d8vz8g803=puair2x8=5bsn70r^k0ot_'
+SECRET_KEY = os.getenv('SECRET_KEY', 'change-me')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.getenv('DEBUG', 0)))
 
-ALLOWED_HOSTS = []
+# os.getenv('VARIAVEL_RANDOLA','') esquerda = variável direita = default
+
+ALLOWED_HOSTS = [
+  h.strip() for h in os.getenv('ALLOWED_HOSTS', '').split(',')
+  if h.strip()
+]  # h = se refere ao host
 
 
 # Application definition
@@ -76,8 +83,13 @@ WSGI_APPLICATION = 'project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': os.getenv('DB_ENGINE', 'change-me'),
+        'NAME': os.getenv('POSTGRES-DB', 'change-me'),
+        'USER': os.getenv('POSTGRES-USER', 'change-me'),
+        'PASSWORD': os.getenv('POSTGRES-PASSWORD', 'change-me'),
+        'HOST': os.getenv('POSTGRES-HOST', 'change-me'),
+        'PORT': os.getenv('POSTGRES-PORT', 'change-me'),
+
     }
 }
 
@@ -104,9 +116,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
@@ -116,7 +128,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+# /data/web/static
+STATIC_ROOT = DATA_DIR / 'static'
+
+MEDIA_URL = '/media/'
+# /data/web/media
+MEDIA_ROOT = DATA_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
